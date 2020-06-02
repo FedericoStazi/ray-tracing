e5758ed009cd79c9106666562f65f297bb257b30
FedericoStazi <stazi.federico@gmail.com>
2020-06-02 21:30:35 +0100

    Refactor Camera

--- Camera/RealCamera.cpp
+++ Camera/RealCamera.cpp
@@ -143,33 +143,3 @@ std::pair<Color, double> RealCamera::cast_n_rays(int height, int width, int j, i
     return {result, var};
 
 }
-
-//TODO make separate class
-//check if randomness on pixel position is needed
-Color RealCamera::cast_ray(Vector2 position, double time) {
-
-    std::random_device rd;
-    std::minstd_rand gen(rd());
-
-    std::uniform_real_distribution<> dis(-0.5, 0.5);
-    std::uniform_real_distribution<> time_dis(0, shutter_speed);
-
-    double x = position.x()-0.5;
-    double y = position.y()-0.5;
-
-    double ray_time = time + time_dis(gen);
-
-    Vector3 focal_point = get_reference_frame().from_plane(
-            Vector2(aperture_size * dis(gen), aperture_size * dis(gen)), ray_time)
-        .add(get_direction(ray_time).scale(aperture_distance));
-
-    Color ray_color = get_scene().cast_ray(
-        Line::between_points(
-            focal_point,
-            get_reference_frame().from_plane(Vector2(get_focal_plane_size() * x, get_focal_plane_size() * y), ray_time)
-                .add(get_direction(ray_time).scale(get_focal_plane_distance()))),
-        _reflections + 1, time);
-
-    return ray_color;
-
-}
